fc898d1012c86005a07311bd0ab48ef252163891
freyja-wion <1084633572@qq.com>
2021-07-12 15:26:52 +0800

    lodash

--- miao/lodash/freyja_wion-lodash.js
+++ miao/lodash/freyja_wion-lodash.js
@@ -1,3 +1,19 @@
 var freyja_wion = function () {
-  function chunk() {}
+  function chunk(array, size) {
+    var arr = [];
+    var j = 0;
+    for (let i = 0; i < Math.ceil(array.length / size); i++) {
+      var index = 0;
+      arr[i] = [];
+      for (; j < array.length; j++) {
+        arr[i].push(array[j]);
+        index++;
+        if (index == size) {
+          j++;
+          break;
+        }
+      }
+    }
+    return arr;
+  }
 };
